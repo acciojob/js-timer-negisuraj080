d01537e11b918ca1d6f6d4fa16dc8bd3d9b42e73
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-23 23:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // write js code here if required
+function updateTimer() {
+        // Get the current date and time
+        var currentDate = new Date();
+
+        // Extract date components
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth() + 1; // Months are 0-indexed
+        var year = currentDate.getFullYear();
+
+        // Extract time components
+        var hours = currentDate.getHours();
+        var minutes = currentDate.getMinutes();
+        var seconds = currentDate.getSeconds();
+
+        // Format the timer string
+        var timerString = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+
+        // Display the timer in the HTML
+        document.getElementById('timer').textContent = timerString;
+    }
+
+    // Update the timer every second (1000 milliseconds)
+    setInterval(updateTimer, 1000);
+
+    // Initial update to display the timer immediately
+    updateTimer();
